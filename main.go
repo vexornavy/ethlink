9ff99b298b133ec0ad0e3325c960e5273018faa4
vexornavy <24568227+vexornavy@users.noreply.github.com>
2017-12-27 18:01:04 +0200

    templates, handlers, layout

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+  "html/template"
+  "log"
   "net/http"
   "os"
   )
+
+//cache our pages
+var tmpls = template.Must(template.ParseFiles("web/index.html", "web/login.html"))
+
 func main() {
   port := os.Getenv("PORT")
   if port == "" {
     port = "8080"
   }
-  http.Handle("/",http.FileServer(http.Dir("public")))
+  http.Handle("/css/", http.FileServer(http.Dir("web")))
+  http.Handle("/js/", http.FileServer(http.Dir("web")))
+  http.HandleFunc("/login/",loginHandler)
+  http.HandleFunc("/", mainHandler)
   http.ListenAndServe(":"+port, nil)
+}
+
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+  renderTemplate(w, "index.html", nil)
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+  renderTemplate(w, "login.html", nil)
+}
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}){
+  err := tmpls.ExecuteTemplate(w, tmpl, data)
+  if err != nil {
+    //log error and return it
+    log.Println(err.Error())
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
