a0b6eef7ccb8c06f5a1d0ee373675214eeb40f3f
vexornavy <24568227+vexornavy@users.noreply.github.com>
2018-02-12 21:57:56 +0200

    force ssl

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
   "log"
   "net/http"
   "os"
+
+  //"github.com/ethereum/go-ethereum/common"
+  //"github.com/ethereum/go-ethereum/crypto"
   )
 
 //cache our pages
@@ -15,20 +18,44 @@ func main() {
   if port == "" {
     port = "8080"
   }
+
   http.Handle("/css/", http.FileServer(http.Dir("web")))
   http.Handle("/js/", http.FileServer(http.Dir("web")))
-  http.HandleFunc("/login/",loginHandler)
+  http.HandleFunc("/login/", loginHandler)
   http.HandleFunc("/", mainHandler)
   http.ListenAndServe(":"+port, nil)
 }
 
+//force SSL helper when running on heroku
+//code source: github.com/jonahgeorge/force-ssl-heroku
+func forceSsl(w http.ResponseWriter, r *http.Request) bool {
+  if os.Getenv("FORCE_SSL") == "TRUE" {
+    if r.Header.Get("x-forwarded-proto") != "https" {
+      sslUrl := "https://" + r.Host + r.RequestURI
+      http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
+      return true
+      }
+    }
+  return false
+}
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
+  //force SSL on heroku
+  redirect := forceSsl(w, r)
+  if redirect {
+    return
+  }
   renderTemplate(w, "index.html", nil)
 }
 func loginHandler(w http.ResponseWriter, r *http.Request) {
+  //force SSL on heroku
+  redirect := forceSsl(w, r)
+  if redirect {
+    return
+  }
   renderTemplate(w, "login.html", nil)
 }
+
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}){
   err := tmpls.ExecuteTemplate(w, tmpl, data)
   if err != nil {
